bcbd35d1f538937c59edb075bef4b1346a326404
Stuart Bowes <stuartbowes2000@gmail.com>
2021-05-27 11:37:16 +0100

    Create AllFlagsPage with ListView containing Flag Countries, plus update deprecated code.

---
+++ FlagFacts/AllFlagsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using FlagData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlagFacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlagsPage : ContentPage
+    {
+        public AllFlagsPage()
+        {
+            BindingContext = DependencyService.Get<FlagDetailsViewModel>();
+            InitializeComponent();
+        }
+    }
+}
